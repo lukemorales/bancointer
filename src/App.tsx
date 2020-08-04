332241fbc61a872dace3e5d5bbecfff9fa7f431a
Luke Morales <lukemorales@live.com>
2020-08-04 20:07:32 -0300

    :art: chore: organize page and components structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
-import Header from './components/Header';
-import Gradient from './components/Gradient';
+import Dashboard from './pages/Dashboard';
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <Header />
-    <Gradient />
+    <Dashboard />
     <GlobalStyles />
   </ThemeProvider>
 );
