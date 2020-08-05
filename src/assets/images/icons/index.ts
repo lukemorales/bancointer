01ecc582316b15a4ef2c48db4e235ddc1d85f916
Luke Morales <lukemorales@live.com>
2020-08-04 23:12:28 -0300

    :bento: chore: add icons

---
+++ src/assets/images/icons/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as BarChartIcon } from './bar-chart.svg';
+export { ReactComponent as DollarSignIcon } from './dollar-sign.svg';
+export { ReactComponent as PieChartIcon } from './pie-chart.svg';
+export { ReactComponent as ShieldIcon } from './shield.svg';
+export { ReactComponent as SmartphoneIcon } from './smartphone.svg';
+export { ReactComponent as UmbrellaIcon } from './umbrella.svg';
