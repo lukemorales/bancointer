e509a8b83ea43d8b7d12fb296c0424b2ac5e8fe9
Luke Morales <lukemorales@live.com>
2020-08-18 22:19:01 -0300

    :sparkles: feat: add line chart do account summary

--- src/pages/Dashboard/MainContent/AccountSummary/index.tsx
+++ src/pages/Dashboard/MainContent/AccountSummary/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 import { FiCreditCard, FiFileText, FiEye, FiEyeOff } from 'react-icons/fi';
+import { ResponsiveLine } from '@nivo/line';
+import { useTheme } from 'styled-components';
 
+import { lineChartData } from '../../../../resources';
 import {
   Container,
   Card,
@@ -10,14 +13,26 @@ import {
   LeftData,
   RightData,
   DataValue,
+  CustomTooltip,
 } from './styles';
 import { PlataformaPaiIcon } from '../../../../assets/images/icons';
 import CreditCardIllustration from '../../../../assets/images/illustrations/card-illustration.png';
 import Button from '../../../../components/Button';
 
+type ChartValue = number | React.ReactText | undefined;
+
+const formatChartValue = (value: ChartValue): string => `${value || 0}%`;
+
 const AccountSummary: React.FC = () => {
   const [displayStatement, setDisplayStatement] = useState(true);
   const [displayInvestments, setDisplayInvestments] = useState(true);
+  const [investmentGrowth, setInvestmentGrowth] = useState(() => {
+    const [investments] = lineChartData;
+    const { y } = investments.data[investments.data.length - 1];
+    return formatChartValue(y);
+  });
+
+  const { colors } = useTheme();
 
   return (
     <Container>
@@ -78,12 +93,54 @@ const AccountSummary: React.FC = () => {
         </Header>
 
         <DataWrapper>
-          <LeftData>Gráfico</LeftData>
+          <LeftData>
+            <ResponsiveLine
+              data={lineChartData}
+              useMesh
+              enableArea
+              enableCrosshair={false}
+              curve="cardinal"
+              margin={{ top: 8, right: 8, bottom: 20, left: 8 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false,
+              }}
+              tooltip={({ point }) => {
+                return (
+                  <CustomTooltip>
+                    {formatChartValue(point.data.yFormatted)}
+                  </CustomTooltip>
+                );
+              }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                orient: 'bottom',
+                tickSize: 0,
+                tickPadding: 8,
+                tickRotation: 0,
+              }}
+              axisLeft={null}
+              colors={colors.success}
+              lineWidth={1.5}
+              pointSize={8}
+              pointColor={colors.success}
+              pointLabel="y"
+              pointLabelYOffset={-12}
+              enableGridY={false}
+            />
+          </LeftData>
           <RightData>
             <span>Total investido</span>
             <DataValue>{displayInvestments ? 'R$ 5.750,00' : '---'}</DataValue>
             <span>Evolução no mês</span>
-            <DataValue>{displayInvestments ? '20%' : '---'}</DataValue>
+            <DataValue>
+              {displayInvestments ? investmentGrowth : '---'}
+            </DataValue>
           </RightData>
         </DataWrapper>
       </Card>
