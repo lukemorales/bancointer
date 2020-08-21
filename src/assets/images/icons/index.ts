0577083d653f7cea9f4fe02259b3654c501c87b1
Luke Morales <lukemorales@live.com>
2020-08-20 21:15:26 -0300

    :bento: chore: add bank products icons

--- src/assets/images/icons/index.ts
+++ src/assets/images/icons/index.ts
@@ -1,2 +1,7 @@
+export { ReactComponent as BasketballIcon } from './basketball.svg';
 export { ReactComponent as BoletoIcon } from './boleto.svg';
+export { ReactComponent as CarIcon } from './car.svg';
+export { ReactComponent as HomeIcon } from './home.svg';
 export { ReactComponent as PlataformaPaiIcon } from './plataforma-pai.svg';
+export { ReactComponent as SmartphoneIcon } from './smartphone.svg';
+export { ReactComponent as TicketIcon } from './ticket.svg';
