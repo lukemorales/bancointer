be2ff183f6202b604315ab000dd7bef7f8bf3ac3
Luke Morales <lukemorales@live.com>
2020-08-20 21:15:26 -0300

    :bento: chore: add bank products icons

---
+++ src/resources/bank-products.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+  HomeIcon,
+  TicketIcon,
+  SmartphoneIcon,
+  CarIcon,
+  BasketballIcon,
+} from '../assets/images/icons';
+
+export default {
+  Casa: {
+    icon: <HomeIcon />,
+  },
+  Entretenimento: {
+    icon: <TicketIcon />,
+  },
+  Tecnologia: {
+    icon: <SmartphoneIcon />,
+  },
+  Transporte: {
+    icon: <CarIcon />,
+  },
+  Esporte: {
+    icon: <BasketballIcon />,
+  },
+};
