1628b61a553d06a5db49f74c5c3a5cc537802de4
Luke Morales <lukemorales@live.com>
2020-08-28 01:40:00 -0300

    :sparkles: feat: create layouts

---
+++ src/pages/_layouts/auth/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Container = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 3.2rem;
+  position: relative;
+`;
