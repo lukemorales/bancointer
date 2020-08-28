b964f454bbcaa0c91f8a67a82fd9bb0e26f6faa1
Luke Morales <lukemorales@live.com>
2020-08-28 01:43:04 -0300

    :sparkles: feat: add navigation to app

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+import PrivateRoute from './PrivateRoute';
+
+import Welcome from '~/pages/Welcome';
+import Dashboard from '~/pages/Dashboard';
+
+const Routes: React.FC = () => {
+  const location = useLocation();
+  const { pathname } = location;
+
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={pathname}>
+        <Route path="/" exact component={Welcome} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    </AnimatePresence>
+  );
+};
+
+export default Routes;
