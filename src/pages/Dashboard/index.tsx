ae463038ec4e20829b7030803f45bbb9d5e9f170
Luke Morales <lukemorales@live.com>
2020-08-28 01:40:00 -0300

    :sparkles: feat: create layouts

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,39 +1,33 @@
 import React from 'react';
 
-import Header from '../../components/Header';
 import { Container, Wrapper } from './styles';
 import SideBar from './SideBar';
 import MainContent from './MainContent';
-import Footer from '../../components/Footer';
+import DefaultLayout from '../_layouts/default';
 
 const animation = {
   unMounted: { opacity: 0 },
   mounted: {
     opacity: 1,
-    // transition: {
-    //   when: 'beforeChildren',
-    //   staggerChildren: 0.2,
-    // },
   },
 };
 
 const Dashboard: React.FC = () => {
   return (
-    <>
-      <Header />
+    <DefaultLayout>
       <Container>
         <Wrapper
           variants={animation}
           initial="unMounted"
           animate="mounted"
+          exit="unMounted"
           transition={{ duration: 1.5 }}
         >
           <SideBar />
           <MainContent />
         </Wrapper>
       </Container>
-      <Footer />
-    </>
+    </DefaultLayout>
   );
 };
 
