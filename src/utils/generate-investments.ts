1240749b2988f2994b6f51865149c73eb5c9b79b
Luke Morales <lukemorales@live.com>
2020-09-03 22:09:19 -0300

    :recycle: chore: generate hidden data for charts

--- src/utils/generate-investments.ts
+++ src/utils/generate-investments.ts
@@ -21,23 +21,29 @@ export interface Investments {
   timeline: Array<GeneratedInvestment>;
 }
 
-const generateInvestments = (): Investments => {
+export interface GenerateInvestments {
+  (visible?: boolean): Investments;
+}
+
+const generateInvestments: GenerateInvestments = (visible = true) => {
   const currentMonthsInterval = generateMonthsInterval();
 
   const chartData = currentMonthsInterval.map((month) => ({
     x: month,
-    y: +faker.finance.amount(2, 37),
+    y: visible ? +faker.finance.amount(5, 37) : 0,
   }));
+
+  const timeline = [
+    {
+      id: 'investments',
+      color: theme.colors.success,
+      data: chartData,
+    },
+  ];
 
   return {
     amount: formatCurrency(+faker.finance.amount(3500, 35000, 2)),
-    timeline: [
-      {
-        id: 'investments',
-        color: theme.colors.success,
-        data: chartData,
-      },
-    ],
+    timeline,
   };
 };
 
