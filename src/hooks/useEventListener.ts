d47db82bcf82ff76f1a4ca06b53a708e01a0e869
Luke Morales <lukemorales@live.com>
2020-09-03 21:07:18 -0300

    :sparkles: feat: create useEventListener hook

---
+++ src/hooks/useEventListener.ts
@@ -0,0 +1,42 @@
+/* eslint consistent-return: 0 */
+import { useEffect, useRef } from 'react';
+
+interface EventListenerOptions {
+  enabled?: boolean;
+  target?: GlobalEventHandlers;
+}
+
+interface EventListenerHook {
+  (
+    eventType: keyof GlobalEventHandlersEventMap,
+    handler: (event: Event | MouseEvent) => void,
+    option: EventListenerOptions,
+  ): void;
+}
+
+const useEventListener: EventListenerHook = (eventType, handler, options) => {
+  const { enabled = true, target = document } = options || {};
+  const handlerRef = useRef(handler);
+
+  useEffect(() => {
+    handlerRef.current = handler;
+  });
+
+  useEffect(() => {
+    if (!enabled) {
+      return;
+    }
+
+    function internalHandler(e: Event): void {
+      return handlerRef.current(e);
+    }
+
+    target.addEventListener(eventType, internalHandler);
+
+    return () => {
+      target.removeEventListener(eventType, internalHandler);
+    };
+  }, [eventType, enabled, target]);
+};
+
+export default useEventListener;
